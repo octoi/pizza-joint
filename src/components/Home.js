8831ab5fbde5954358add866a6efc356620de17c
fadhilsaheer <fadhilsaheer@gmail.com>
2021-07-20 12:56:30 +0530

    key frames

--- src/components/Home.js
+++ src/components/Home.js
@@ -2,6 +2,14 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 import { motion } from 'framer-motion';
 
+const buttonVariants = {
+    hover: {
+        scale: [1, 1.1, 1, 1.1, 1],
+        textShadow: "0px 0px 8px rgb(255,255,255)",
+        boxShadow: "0px 0px 8px rgb(255,255,255)"
+    }
+}
+
 const Home = () => {
     return (
         <motion.div className="home container"
@@ -14,11 +22,8 @@ const Home = () => {
             </motion.h2>
             <Link to="/base">
                 <motion.button
-                    whileHover={{
-                        scale: 1.1,
-                        textShadow: "0px 0px 8px rgb(255,255,255)",
-                        boxShadow: "0px 0px 8px rgb(255,255,255)"
-                    }}
+                    variants={buttonVariants}
+                    whileHover="hover"
                 >
                     Create Your Pizza
                 </motion.button>
